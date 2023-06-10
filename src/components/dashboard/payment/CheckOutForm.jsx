bf85f1a1d5de69ca2c8d85b8e4be3d4ce8cec618
shakilkhan2 <dev.shakilkhan@gmail.com>
2023-06-11 02:38:31 +0600

    got amount for payment

--- src/components/dashboard/payment/CheckOutForm.jsx
+++ src/components/dashboard/payment/CheckOutForm.jsx
@@ -1,53 +1,78 @@
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import { useState } from "react";
+
+const CheckOutForm = ({ price }) => {
+  console.log(price);
+  const stripe = useStripe();
+  const elements = useElements();
+  const [cardError, setCardError] = useState("");
 
 
-const CheckOutForm = () => {
-    const stripe = useStripe();
-    const elements = useElements();
 
-const handleSubmit =async (event) => {
-    event.preventDefault()
 
-    if(!stripe || !elements){
-        return
+  
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    if (!stripe || !elements) {
+      return;
     }
 
     const card = elements.getElement(CardElement);
-    if(card === null){
-        return
+    if (card === null) {
+      return;
     }
-    console.log('card', card)
 
-}
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: "card",
+      card,
+    });
 
+    if (error) {
+      console.log("error", error);
+      setCardError(error.message);
+    } else {
+      setCardError("");
+      console.log("payment method", paymentMethod);
+    }
+  };
 
-    return (
-        <div>
-             
-
-              <form className="border bg-base-100 shadow-2xl border-amber-700 p-4 pb-12 rounded-md" onSubmit={handleSubmit}>
-      <CardElement className="border border-amber-700 p-2 rounded-md"
-        options={{
-          style: {
-            base: {
-              fontSize: '16px',
-              color: '#424770',
-              '::placeholder': {
-                color: '#aab7c4',
+  return (
+    <div>
+      <form
+        className="border bg-base-100 shadow-2xl border-amber-700 p-4 pb-12 rounded-md"
+        onSubmit={handleSubmit}
+      >
+        <CardElement
+          className="border border-amber-700 p-2 rounded-md"
+          options={{
+            style: {
+              base: {
+                fontSize: "16px",
+                color: "#424770",
+                "::placeholder": {
+                  color: "#aab7c4",
+                },
+              },
+              invalid: {
+                color: "#9e2146",
               },
             },
-            invalid: {
-              color: '#9e2146',
-            },
-          },
-        }}
-      />
-      <button type="submit" className="  px-4 my-4 py-1  rounded-md  bg-amber-600 text-white hover:bg-amber-700 mx-2 font-semibold" disabled={!stripe}>
-        Pay
-      </button>
-    </form>
-        </div>
-    );
+          }}
+        />
+        <button
+          type="submit"
+          className="  px-4 my-4 py-1  rounded-md  bg-amber-600 text-white hover:bg-amber-700 mx-2 font-semibold"
+          disabled={!stripe}
+        >
+          Pay
+        </button>
+      </form>
+      {cardError && (
+        <p className="text-sm text-red-600 mt-4 text-center"> {cardError} </p>
+      )}
+    </div>
+  );
 };
 
 export default CheckOutForm;
--- src/components/dashboard/payment/Payment.jsx
+++ src/components/dashboard/payment/Payment.jsx
@@ -1,15 +1,20 @@
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 import CheckOutForm from "./CheckOutForm";
+import useCart from "../../hooks/useCart";
 
 // TODO: provide publishable key
 const stripePromise = loadStripe(import.meta.env. VITE_PAYMENT_GATEAWAY_PK)
 const Payment = () => {
+const [cart] = useCart()
+const total = cart.reduce((sum, course) => course.price + sum, 0);
+const price = parseFloat(total.toFixed(2));
+
     return (
         <div className="w-[50%]">
              <h1 className="text-2xl font-semibold text-center text-amber-600 mb-8">Payment Card</h1>
              <Elements stripe={stripePromise}>
-                <CheckOutForm/>
+                <CheckOutForm price={price}/>
              </Elements>
         </div>
     );
